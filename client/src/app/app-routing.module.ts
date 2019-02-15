eac2eb273105c512e10b938a77453187c7ba86bd
dsvirido <dimanok88@gmail.com>
2019-02-15 13:25:23 +0100

    installed angular

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+
+const appRoute: Routes = [
+    {path: "places", component: PlacesComponent}
+];
+
+@NgModule({
+    imports: []
+})
+
+export class AppRoutingModule {
+
+}
