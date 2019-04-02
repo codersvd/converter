3793b3c2fa5fb951e4c35c8eb9d8bf9343c330e1
codersvd <dimanok88@gmail.com>
2019-04-02 22:55:58 +0300

    added tabs for places

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlacesComponent } from './places/places.component';
+import {PlaceComponent} from './places/place/place.component';
 
 const appRoute: Routes = [
-    { path: 'places', component: PlacesComponent }
+    { path: 'places', component: PlacesComponent },
+    { path: 'place/:id', component: PlaceComponent }
 ];
 
 @NgModule( {
