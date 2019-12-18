259d98a3453fd2d1866deea42eb55a38a6b51e75
dsvirido <Dmitrii.Sviridov@t-systems.ru>
2019-12-18 11:56:44 +0300

    new

--- client/src/app/modules/login/login.component.ts
+++ client/src/app/modules/login/login.component.ts
@@ -1,37 +1,42 @@
-import { Component, OnInit }                  from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.less']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
-    email: FormControl = new FormControl(null, [Validators.email, Validators.required]);
-    password: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, [
+    Validators.email,
+    Validators.required
+  ]);
+  password: FormControl = new FormControl(null, Validators.required);
 
-    loginForm: FormGroup;
+  loginForm: FormGroup;
 
-    constructor() {}
+  constructor() {}
 
-    ngOnInit() {
-        this.loginForm = new FormGroup({
-            'email': this.email,
-            'password': this.password
-        });
-    }
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: this.email,
+      password: this.password
+    });
+  }
 
-    onSubmit() {
-        console.log( this.loginForm );
-    }
+  onSubmit() {
+    console.log(this.loginForm);
+  }
 
-    getErrorEmail( email: FormControl ) {
-        return this.email.hasError('required') ? 'You must enter a value' :
-            this.email.hasError('email') ? 'Not a valid email' :
-                '';
-    }
+  getErrorEmail(email: FormControl) {
+    return this.email.hasError('required')
+      ? 'You must enter a value'
+      : this.email.hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
 
-    getErrorPassword( password: FormControl ) {
-        return this.password.hasError('required') ? 'You must enter a value' : '';
-    }
+  getErrorPassword(password: FormControl) {
+    return this.password.hasError('required') ? 'You must enter a value' : '';
+  }
 }
